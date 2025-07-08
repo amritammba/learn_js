5fedec98b195719533e5afc1f14441fdd6be28cd
amritammba <amritarao26046@gmail.com>
2025-07-08 18:25:48 +0530

    type conversion and string functions

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 //alert("hello world");
-var name1 = prompt("Enter your Name","Arvind");
-console.log("Hello " + name1);
+// var name1 = prompt("Enter your Name","Arvind");
+// console.log("Hello " + name1);
+// let a;
+// a=25;
+// console.log(a,typeof a);
+
+//type conversion
+
+// let age = parseInt(prompt("Enter your age", "25"));
+// console.log(age, typeof age);
+// let a,b;
+// a= Number("22");
+// b= 10;
+// console.log(a+b, typeof (a+b));
+
+// strings
+
+// let c;
+// c = "she can \'t run";
+// console.log(c);
+
+// let text = "krishna is a good teacher";
+// let c;
+// c = text.substring(4);       // negative indexing willnot work in substring
+// console.log(c);
+
+// spiting 
+let c;
+c = "krishna is a good teacher";
+console.log(c.split(" ")); // split by space
+console.log("split: "+c);
+console.table(c.split(" ")); // split by space
+
+
